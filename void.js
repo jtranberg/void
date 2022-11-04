381549a6a5d1f52e2db28a34bdf91c9673fb0228
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-11-04 15:02:01 -0700

    added ..never..as type

--- void.js
+++ void.js
@@ -7,3 +7,17 @@ function printTwice(msg) {
     console.log(msg);
     console.log(msg);
 }
+var neverStop = function () {
+    while (true) {
+        console.log("i am still going");
+    }
+};
+function makeError(msg) {
+    throw new Error(msg);
+}
+function gameloop() {
+    while (true) {
+        console.log("game loop running!");
+    }
+    return true; // error shows as never to be returned
+}
--- void.ts
+++ void.ts
@@ -5,6 +5,23 @@ const annonUser = (num:number): void => {
 }
 
 function printTwice(msg:string): void  {     //does not return anything
-    console.log(msg)
-    console.log(msg)
+    console.log(msg);
+    console.log(msg);
+}
+
+const neverStop = (): never => {
+    while(true) {
+        console.log("i am still going");
+    }
+}
+
+function makeError(msg: string): never {
+    throw new Error(msg);
+}
+
+function gameloop(): never {
+  while(true) {
+    console.log("game loop running!");
+  }
+  return true     // error shows as never to be returned
 }
